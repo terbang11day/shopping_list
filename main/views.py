06d878f9e0ab2805255d011e7e6585a6286c3503
terbang11day <thariqziyad48@gmail.com>
2023-10-04 13:52:34 +0700

    Menyelesaikan tutorial 5

--- main/views.py
+++ main/views.py
@@ -1,31 +1,29 @@
 import datetime
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
 from .models import Product
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages  
-
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/login')
 def show_main(request):
     products = Product.objects.filter(user=request.user)
+
     context = {
         'name': request.user.username,
-        'class': 'PBP   B',
+        'class': 'PBP B',
         'products': products,
-        'last_login': request.COOKIES['last_login'],
+        #'last_login': request.COOKIES['last_login'],
     }
+
     return render(request, "main.html", context)
 
 def create_product(request):
@@ -78,8 +76,6 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main")) 
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-        else:
-            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
     context = {}
     return render(request, 'login.html', context)
 
@@ -90,14 +86,10 @@ def logout_user(request):
     return response
 
 def edit_product(request, id):
-    # Get product berdasarkan ID
     product = Product.objects.get(pk = id)
-
-    # Set product sebagai instance dari form
     form = ProductForm(request.POST or None, instance=product)
 
     if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
@@ -105,9 +97,26 @@ def edit_product(request, id):
     return render(request, "edit_product.html", context)
 
 def delete_product(request, id):
-    # Get data berdasarkan ID
     product = Product.objects.get(pk = id)
-    # Hapus data
     product.delete()
-    # Kembali ke halaman awal
+   
     return HttpResponseRedirect(reverse('main:show_main'))
+
+def get_product_json(request):
+    product_item = Product.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
