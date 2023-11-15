69c68910227992e98f920c1a6bf89395a1462873
terbang11day <thariqziyad48@gmail.com>
2023-11-15 12:14:57 +0700

    test

--- main/urls.py
+++ main/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user, edit_product, delete_product
-from main.views import get_product_json, add_product_ajax
+from main.views import show_main, create_product, show_xml, show_json
+from main.views import register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax
+
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product', create_product, name='create_product'),
-    path('xml/', show_xml, name='show_xml'),
-    path('json/', show_json, name='show_json'), 
-    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
-    path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
+    path('xml/', show_xml, name='show_xml'), 
+    path('json/', show_json, name='show_json'),
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'), 
     path('logout/', logout_user, name='logout'),
     path('edit-product/<int:id>', edit_product, name='edit_product'),
     path('delete/<int:id>', delete_product, name='delete_product'),
